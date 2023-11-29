0fe7f47dc65e65f588f55b2093bf53538ef5eeb7
Robert Grizzell <robert@grizzell.me>
2023-11-28 23:30:26 -0600

    Add support for Debian 12 Bookworm

--- manifests/params.pp
+++ manifests/params.pp
@@ -16,7 +16,7 @@ class easy_ipa::params {
     }
     'Debian': {
       case $facts['os']['distro']['codename'] {
-        /(trusty|xenial|bionic|buster|focal|bullseye|jammy)/: { $ipa_client_package_ensure = 'present' }
+        /(trusty|xenial|bionic|buster|focal|bullseye|jammy|bookworm)/: { $ipa_client_package_ensure = 'present' }
         /(stretch)/:                                          { $ipa_client_package_ensure = 'absent' }
         default:                                              { fail('ERROR: unsupported operating system') }
       }
